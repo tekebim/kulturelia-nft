8990d755eb9622dfe5ca05672dc0da0ca66c4734
Michael R <tekmedias@gmail.com>
2022-06-15 14:33:26 +0200

    :coffin: Clean default header component

--- components/Header.vue
+++ components/Header.vue
@@ -2,7 +2,7 @@
     <div>
         <header>
             <nav class="nav">
-                <a href="#" class="nav__item" @click.prevent.self @mouseover="isUser = true"  @mouseleave="isUser = false">
+                <a href="#" class="nav__item" @click.prevent.self>
                   Accueil
                 </a>
             </nav>
@@ -18,15 +18,8 @@ import { Vue, Component } from 'nuxt-property-decorator'
 
     data(){
         return {
-            idCollection:   false,
-            isLight:   false,
-            isInformation: false,
-            isLanguage:  false,
-            isUser:  false
         }
     }
-
-
 })
 
 export default class Header extends Vue {}
@@ -36,37 +29,4 @@ export default class Header extends Vue {}
     header{
         @apply h-full shadow-lg bg-backgroundBlock-light dark:bg-backgroundBlock-dark;
     }
-
-    .nav{
-        @apply grid;
-    }
-
-    .nav__item{
-        @apply grid place-content-center relative;
-        height: 80px;
-
-        svg{
-            @apply duration-200
-        }
-    }
-
-    .nav__item:hover{
-        svg{transform: scale(0.9)}
-    }
-    .nav__item:first-child{
-        &:hover{transform: none}
-    }
-
-    .dropDownMenu{
-        @apply absolute top-0 left-20;
-    }
-
-    .nav__item__text{
-        @apply hidden;
-    }
-
-    .nav__item svg{
-        @apply h-7 w-7;
-    }
-
 </style>
