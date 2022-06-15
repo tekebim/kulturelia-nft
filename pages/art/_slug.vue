4a5867667531fc63eb522a1d94d5f8ada15b6784
Michael R <tekmedias@gmail.com>
2022-06-15 16:42:08 +0200

    :sparkles: Add art single details page

---
+++ pages/art/_slug.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>{{ slug }}</h1>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const slug = params.slug // When calling /abc the slug will be "abc"
+    return { slug }
+  }
+}
+</script>
