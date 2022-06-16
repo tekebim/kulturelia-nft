a10bf1121b1ac5a6521566233bc2cd40ea8b23ea
papaya-industry <maretvincent@protonmail.com>
2022-06-16 15:18:34 +0200

    feat: sticky menu

--- components/menu/Mobile.vue
+++ components/menu/Mobile.vue
@@ -1,9 +1,12 @@
 <template>
-  <nav class="menu">
-    <Icon name="burger" :size="2" />
-    <Icon name="kulturelia" :size="2" />
-    <Icon name="search" :size="2" />
-  </nav>
+  <div class="menu-wrapper">
+    <nav class="menu">
+      <Icon name="burger" :size="2" />
+      <Icon name="kulturelia" :size="2" />
+      <Icon name="search" :size="2" />
+    </nav>
+    <MenuLinks class="links" />
+  </div>
 </template>
 
 <script lang="ts">
@@ -21,12 +24,21 @@ export default class Menu extends Vue {}
 </script>
 
 <style lang="scss" scoped>
+.menu-wrapper {
+  position: relative;
+}
+
 .menu {
+  background: white;
   width: 100vw;
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding: 1.5rem;
+}
+
+.links {
+  height: 100%;
 }
 </style>
 
