62e4ea633d215ee74f7b4d701a45de120d8f0c3e
Michael R <tekmedias@gmail.com>
2022-06-16 16:34:11 +0200

    :sparkles: Add loader and fix asset card

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,21 @@
     <div class="grid grid-cols-1 md:grid-cols-3 gap-8 m-5">
       <div
         v-for="asset of formatedAssets"
+        v-if="isCollectionLoaded"
         :key="asset.id"
-        class="
-          grid_block
-          max-w-sm
-          bg-white
-          rounded-lg
-          border border-gray-200
-          shadow-md
-          dark:bg-gray-800 dark:border-gray-700
-        "
+        class="asset-card grid_block"
       >
-        <nuxt-link :key="asset.id" :to="`/art/${asset.asset_contract.address}/${asset.token_id}`" class=" inline-block w-full" style="aspect-ratio: 1/1">
+        <nuxt-link :key="asset.id" :to="`/art/${asset.asset_contract.address}/${asset.token_id}`"
+                   class="card-link inline-block w-full">
           <img
             :alt="asset.name"
             :src="asset.image_url"
             class="rounded-t-lg w-full h-full object-cover"
           />
-        </nuxt-link>
-        <div class="p-5">
-          <h5
-            class="
+          <div class="p-5">
+            <h5
+              class="
+              card__title
               mb-2
               text-2xl
               font-bold
@@ -31,20 +25,24 @@
               text-gray-900
               dark:text-white
             "
-          >
-            {{ asset.name }}
-          </h5>
-          <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Kulturelia, Paris 13ème arrondissement
-          </p>
-        </div>
+            >
+              {{ asset.name }}
+            </h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Kulturelia, Paris 13ème arrondissement
+            </p>
+          </div>
+        </nuxt-link>
+      </div>
+      <div v-else class="card-loader">
+        <LoaderIcon/>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'nuxt-property-decorator'
+import {Vue, Component} from 'nuxt-property-decorator'
 import {
   getAssets,
   getCollection,
@@ -54,14 +52,17 @@ import {
 } from '~/service/openSea'
 
 import slugify from '~/helpers/slugify'
+import LoaderIcon from "~/components/ui/Icon/Loader.vue";
 
 const DEFAULT_IMG =
   'https://img.seadn.io/files/d67477e51780cdeaf45fd96d97b1dfa9.png?fit=max&auto=format&h=720&w=720'
-
-@Component
+@Component({
+  components: {LoaderIcon}
+})
 export default class HomePage extends Vue {
   collection: Collection | null = null
   assets: Asset[] | null = null
+  isCollectionLoaded: Boolean = false;
 
   get formatedAssets(): Asset[] | null {
     if (!this.assets) return null
@@ -72,6 +73,7 @@ export default class HomePage extends Vue {
       asset.image_url = asset.image_url ?? DEFAULT_IMG
     }
 
+    this.isCollectionLoaded = true
     return formatedAssets
   }
 
@@ -86,4 +88,23 @@ export default class HomePage extends Vue {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.card-loader {
+  @apply place-self-center p-20;
+}
+
+.asset-card {
+  @apply max-w-sm bg-white rounded-lg border border-gray-200 text-center dark:bg-gray-800 dark:border-gray-700;
+
+  .card-link {
+    &:hover {
+      .card__title {
+        @apply underline
+      }
+
+    }
+
+    aspect-ratio: 1/1;
+  }
+}
+</style>
