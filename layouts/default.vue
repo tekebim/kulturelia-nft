66cd2c42941eca1550a8cf07e823c9dd39c5170b
papaya-industry <maretvincent@protonmail.com>
2022-06-16 10:09:49 +0200

    feat : add dummy nav

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,8 @@
 <template>
-  <Nuxt />
+  <div>
+    <nav>This is a dummy nav</nav>
+    <Nuxt />
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,3 +15,11 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" scoped>
+nav {
+  background: grey;
+  width: 100vw;
+  height: 100px;
+}
+</style>>
